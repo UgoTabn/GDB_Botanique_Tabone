37ce89e2519f25279f7f6ed8d19460173a77a1ce
DavidEtGeorges <59653406+DavidEtGeorges@users.noreply.github.com>
2020-05-11 10:01:32 +0200

    Create Plante.cpp

---
+++ Plante.cpp
@@ -0,0 +1,19 @@
+#ifndef PLANTE_CPP
+#define PLANTE_CPP
+
+#include <string>
+#include "Plante.h"
+#include <iostream>
+
+//int _cout; int _revente; int _engrais; int _taille; int _sante; int _maturite; int _maturiteMax; std::string _name;
+//void virtual inspecter(); Plante(std::string nom);
+//int inspecter();
+
+
+    void Plante::inspecter(){
+        std::cout << "Ceci est une Carte sans type !!!" << std::endl;
+    }
+
+    Plante::Plante(int cout,int _revente,int _engrais,int _taille,int _sante,int _maturite,int _maturiteMax,std::string _name):
+         {}
+#endif
