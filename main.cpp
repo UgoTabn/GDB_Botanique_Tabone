65ece3d7bc9f9065e42e2e8b3dee89d560341945
DavidEtGeorges <59653406+DavidEtGeorges@users.noreply.github.com>
2020-05-11 11:30:53 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Plante.h"
+#include "Plante.cpp"
+//#include "Fleur.h"
+//include "Fleur.cpp"
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+Plante * TEST = new Plante();
+TEST->inspecter();
+TEST->mettreEngrais();
+TEST->inspecter();
+<>
+
+
+
+
+
+    return 0;
+}
